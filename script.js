337a615cce3fb6349b554c1cf9d0c49afc1a53e7
Naomi 💜 <naomiblack@Naomis-MacBook-Air.local>
2017-08-08 09:30:21 -0400

    commit

--- script.js
+++ script.js
@@ -3,27 +3,24 @@ var app = angular.module("simulator", []);
 app.controller("wordController", function($scope) {
 console.log("hello");
 	$scope.words = [];
-	var random = [{text: "why?", style: "blue" }, 
+	$scope.random = [{text: "why?", style: "blue" },
  					 {text: "hungry", style: "blue"},
  					 {text: "sleepy", style: "blue"},
  					 {text: "you're mean", style: "blue"},
  					 {text: "cookie", style: "blue"} ];
  	// take words from "random" array
- 	//add words to view AND words array				 
+ 	//add words to view AND words array
 	$scope.addWord = function () {
-  var x = "";
-  	x.word=random.text[Math.floor(Math.random() * random.length)];
-  $scope.words.push(x);
-  
-  //$scope.words.push($scope.random[Math.floor(Math.random()*5)])
-	//console.log("hi);
-		
-	
-	
-	}	
-	
-	
-	
-	});				 
-	
-	
+
+  $scope.words.push($scope.random[Math.floor(Math.random()*5)])
+	console.log("hi");
+
+	}
+
+$scope.decoratedWord = function(){
+	$scope.words.push($scope.random[Math.floor(Math.random()*5)])
+	console.log("hi");
+
+}
+
+	});
